155c0863d55c1e2e1407d0062c6f79932eadd3fb
kmackatvail <kmack@vailsys.com>
2023-10-03 09:08:38 -0500

    Update mongo.go

--- mongo.go
+++ mongo.go
@@ -62,20 +62,19 @@ func (c *Client) InsertMany(database string, collection string, docs []any) erro
 }
 
 func (c *Client) CountDocuments(database string, collection string, filter interface{}, limit int64) int64 {
-        log.Printf("Count documents")
-        // db := c.client.Database(database)
-        // col := db.Collection(collection)
+        //log.Printf("Count documents")
+        db := c.client.Database(database)
+        col := db.Collection(collection)
         // log.Print(filter_is, filter)
-	// doc := bson.D{}
         // count, err := col.CountDocuments(context.TODO(), filter, options.CountOptions().SetLimit(limit))
 	// count, err := col.CountDocuments(context.TODO(), filter)
-	// count, err := col.CountDocuments(context.TODO(), doc)
-        //if err != nil {
-        //        log.Fatal(err)
-	//	return -1
-        //}
-        // log.Printf("count documents %v", count)
-        return 100
+	count, err := col.CountDocuments(context.TODO(), bson.D{}, options.Count().SetLimit(limit))
+        if err != nil {
+        	panic(err)
+		return -1
+        }
+        //log.Printf("count documents %v", count)
+        return count
 }
 
 func (c *Client) Find(database string, collection string, filter interface{}, limit int64) []bson.M{
